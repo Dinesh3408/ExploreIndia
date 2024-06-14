2cd2b35176556380fb30557a607021fe543bd0dd
Dinesh3408 <davaladinesh34@gmail.com>
2024-06-15 04:36:08 +0530

    Explore Idia

---
+++ src/main/java/com/example/ec/ExploreindiApplication.java
@@ -0,0 +1,14 @@
+package com.example.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreindiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ExploreindiApplication.class, args);
+	}
+
+}
